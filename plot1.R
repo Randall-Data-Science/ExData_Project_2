b0ed3db1afc84e80984c41abedadfcf8eab08384
Randall-dsci <rjzmml+Randall-dsci@gmail.com>
2014-11-19 17:58:29 -0800

    Update spacing of x-axis years to closely match data

--- plot1.R
+++ plot1.R
@@ -1,17 +1,15 @@
 # See notes on libraries at end of documents
-library(data.table)
-library(dplyr)
-library(tidyr) 
+library(data.table); library(dplyr); library(tidyr) 
 
 #load data sets if not already in memory
 if (!exists("NEI") | !exists("SCC")) source("loadData.R")
 
 # Create a copy of NEI table for this part one of the assignment, create factors
-# filter out data, not present for all years, and average data for specific 
-# points within a year
+# filter out data that are not present for all years, and then average data for 
+# specific points within a year
 
-# turn year and obs type into factors
-# create columns for grouping
+# turn year and obs type into factors,
+# then create columns for grouping
 NEI.p1 <- NEI %>% 
     mutate(type = as.factor(type), year = as.factor(year)) %>%
     mutate(obs.id = paste(fips, SCC, type, sep = "-")) %>%
@@ -41,10 +39,10 @@ p1.data <- NEI.p1.aY %>%
     summarise(Emissions = mean(Emissions)) %>%
     mutate(year = as.numeric(as.character(year)))
 
-png(filename = "plot1.png",width = 600, height = 400)
+png(filename = "plot1.png", width = 600, height = 400)
 
-plot(p1.data, type = "b")
-title(main = "Levels of PM_2.5 1999-2008")
+plot(p1.data, type = "b", xaxp = c(1999, 2008, 3))
+title(main = "Levels of PM_2.5 (1999-2008)")
 
 dev.off()
 
